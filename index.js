046d2f581f9ad1363b68863982fb74c35f3b00bd
Darshan Badrakiya <darshan.badrakiya@streebo.com>
2026-04-25 21:14:00 +0530

    Enable Auto-Deploy

--- index.js
+++ index.js
@@ -8,11 +8,15 @@ const VERSION = "v1.0.0";
 
 app.get("/", (req, res) => {
   res.send(`
-    <h1>🚀 CI/CD Demo App</h1>
+    <h1>CI/CD Demo App - Render Update</h1>
     <p>Version: ${VERSION}</p>
     <p>Time: ${new Date()}</p>
   `);
 });
+app.get('/healthz', (req, res) => {
+  res.send('OK');
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
