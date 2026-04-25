9f9150c32d2c389219a7f1608746ef88aca2ad9f
Darshan Badrakiya <darshan.badrakiya@streebo.com>
2026-04-25 20:10:17 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+// Change this version to test CI/CD updates
+const VERSION = "v1.0.0";
+
+app.get("/", (req, res) => {
+  res.send(`
+    <h1>🚀 CI/CD Demo App</h1>
+    <p>Version: ${VERSION}</p>
+    <p>Time: ${new Date()}</p>
+  `);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
