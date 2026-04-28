6e5ced300c9b16ca75406253ef6f45fe7cedbe3b
Darshan Badrakiya <darshan.badrakiya@streebo.com>
2026-04-28 16:36:36 +0530

    Test Hello World

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -6,7 +6,7 @@ describe('Basic API Tests', () => {
   test('GET / should return hello message', async () => {
     const res = await request(app).get('/');
     expect(res.statusCode).toBe(200);
-    expect(res.text).toBe('Hello CI/CD');
+    expect(res.text).toBe('Hello World');
   });
 
   test('GET /healthz should return OK', async () => {
